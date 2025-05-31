cde84c2b1696b52ccae8494aeb6cf6490bc10455
yashesh-akbari <yasheshakbari@gmail.com>
2025-05-31 20:23:19 +0530

    31 may coding challnge complted

---
+++ MAY/MAY_31/q8.js
@@ -0,0 +1,9 @@
+// 8. Check if a number is even or odd.
+// let num=22;
+let num=21;
+if(num%2===0){
+  console.log(num+" is even number");
+}
+else{
+  console.log(num+ " is odd number" );
+}
---
+++ MAY/MAY_31/q9.js
@@ -0,0 +1,8 @@
+// 9. Find the average of an array of numbers. 
+let arr=[1,2,3,4,5];
+let temp=0;
+for(let i=0;i<arr.length;i++){
+  temp=temp+arr[i];
+}
+let answer=temp/arr.length;
+console.log(answer);
